958c76d6ae52915e5de960817d890697fa5e627f
wryte123 <oepkf@foxmail.com>
2024-03-01 21:53:55 +0800

    Add router and Element Plus dependencies, and update main.js and App.vue

--- camp-v3/src/App.vue
+++ camp-v3/src/App.vue
@@ -3,7 +3,7 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
@@ -28,7 +28,3 @@ nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
-
-<script setup>
-
-</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router/index";
+import Element from "element-plus";
+import "element-plus/theme-chalk/index.css";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router);
+app.use(Element);
+app.mount("#app");
