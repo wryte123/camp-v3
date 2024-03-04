f93d02d2078fb52135ff2d58cfdaa4919b573613
wryte123 <oepkf@foxmail.com>
2024-03-04 18:25:30 +0800

    Add Sass and Sass-loader dependencies, update LoginView.vue

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,13 +1,32 @@
 <template>
-  <div class="panel-wrapper">
-    <el-card class="panel"></el-card>
+  <div class="login-container">
+    <h1>登录</h1>
+    <div class="panel-wrapper">
+      <el-card class="panel">
+        <el-form class="login-panel">
+          <el-form-item label="Username" size="large">
+            <el-input v-model="username" placeholder="Username" />
+          </el-form-item>
+          <el-form-item label="Password" size="large">
+            <el-input v-model="password" placeholder="Password" />
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="login">登录</el-button>
+            <el-button type="info">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </el-card>
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ElMessage } from 'element-plus';
 
-<style scoped>
-.panel-wrapper {
-	
+function login() {
+  ElMessage("Hello, Element Plus!");
 }
+</script>
+
+<style lang="scss" scoped>
 </style>
