bc2f7731e452a5707ea9b60240187aa7c330b172
wryte123 <oepkf@foxmail.com>
2024-03-04 11:38:32 +0800

    Fix bug in login functionality

---
+++ src/views/LoginView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="panel-wrapper">
+    <el-card class="panel"></el-card>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.panel-wrapper {
+	
+}
+</style>
