3bb97bac12d7f1c022928665b4c63c0b380ac36a
wryte123 <oepkf@foxmail.com>
2024-03-05 22:40:34 +0800

    Refactor login form in LoginView.vue

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,13 +2,13 @@
   <div class="panel-wrapper">
     <el-card class="panel">
       <h1>登录</h1>
-      <el-form class="login-panel">
+      <el-form class="login-panel" label-position="top">
         <el-form-item label="Username" size="large">
-          <el-input v-model="username" placeholder="Username" />
+          <el-input v-model="user.username" placeholder="Username" />
         </el-form-item>
         <el-form-item label="Password" size="large">
           <el-input
-            v-model="password"
+            v-model="user.password"
             placeholder="Password"
             show-password
             type="password"
@@ -17,7 +17,7 @@
         </el-form-item>
         <el-form-item class="login-btn">
           <el-button type="info">重置</el-button>
-          <el-button type="warning" @click="login">登录</el-button>
+          <el-button type="warning" @click="handelLogin">登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -25,11 +25,31 @@
 </template>
 
 <script setup>
-import { ElMessage } from "element-plus";
+// import { ElMessage } from "element-plus";
+// import { reactive } from "vue";
 
-function login() {
-  ElMessage("Hello, Element Plus!");
-}
+// const user = reactive({
+//   username: "",
+//   password: "",
+// });
+
+// const rules= {
+//   username: [
+//     { required: true, message: "请输入用户名", trigger: "blur" },
+//   ],
+//   password: [
+//     { required: true, message: "请输入密码", trigger: "blur" },
+//   ],
+// };
+
+// const handelLogin = async () => {
+//   try {
+//     const response = await loginUser();
+//     console.log(response);
+//   } catch (error) {
+//     ElMessage.error("登录失败");
+//   }
+// }
 </script>
 
 <style lang="scss" scoped>
