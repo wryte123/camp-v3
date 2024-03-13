9a4d32167af1a04c9b3aa4aec96cd93f65115a6f
wryte <oepkf@foxmail.com>
2024-03-14 01:34:15 +0800

    Add password confirmation field to registration form

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -5,11 +5,20 @@ const user = reactive({
   email: "",
   username: "",
   p: "",
+  vp: "",
 });
 
-const validatep = "";
 
 const rules = {
+  username: [
+    { required: true, message: "请输入用户名", trigger: "blur" },
+    {
+      min: 3,
+      max: 20,
+      message: "用户名长度在 3 到 20 个字符",
+      trigger: "blur",
+    },
+  ],
   email: [
     { required: true, message: "请输入电子邮箱", trigger: "blur" },
     { type: "email", message: "请输入正确的邮箱地址", trigger: "blur" },
@@ -18,30 +27,39 @@ const rules = {
     { required: true, message: "请输入密码", trigger: "blur" },
     { min: 6, max: 20, message: "密码长度在 6 到 20 个字符", trigger: "blur" },
   ],
+  vp: [{ validator: validatePass, trigger: "blur" }],
 };
 
-const handleReg = async () => {
-  
-  return {
-    user,
-    rules,
-    handleReg,
-  };
-}
+const validatePass = (rule, value, callback) => {
+  if (value === "") {
+    callback(new Error("请再次输入密码"));
+  } else if (value !== user.p) {
+    callback(new Error("两次输入密码不一致!"));
+  } else {
+    callback();
+  }
+};
+
+// const handleReg = async () => {
+//   return {
+//     user,
+//     rules,
+//   };
+// };
 </script>
 
 <template>
   <div class="panel-wrapper">
     <el-card class="panel">
       <h1>注册</h1>
-      <el-form class="register-panel" label-position="top">
-        <el-form-item label="用户名" size="large">
+      <el-form class="register-panel" label-position="top" :rules="rules">
+        <el-form-item label="用户名" size="large" prop="username">
           <el-input v-model="user.username" placeholder="Username" />
         </el-form-item>
-        <el-form-item label="电子邮件" size="large">
+        <el-form-item label="电子邮件" size="large" prop="email">
           <el-input v-model="user.email" placeholder="E-mail" />
         </el-form-item>
-        <el-form-item label="密码" size="large">
+        <el-form-item label="密码" size="large" prop="p">
           <el-input
             v-model="user.p"
             placeholder="Password"
@@ -50,10 +68,10 @@ const handleReg = async () => {
             autocomplete="off"
           />
         </el-form-item>
-        <el-form-item label="再次确认密码" size="large">
+        <el-form-item label="确认密码" size="large" prop="vp">
           <el-input
-            v-model="validatep"
-            placeholder="Password"
+            v-model="user.vp"
+            placeholder="Confirmed"
             show-password
             type="password"
             autocomplete="off"
