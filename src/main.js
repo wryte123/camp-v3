4075ceea1ae115c8d39a477f3924a96d0a5102fb
ElectRICdll <2636422098@qq.com>
2024-03-17 15:52:38 +0800

    将elementicon注册为组件

--- src/main.js
+++ src/main.js
@@ -5,8 +5,13 @@ import Element from "element-plus";
 import "element-plus/theme-chalk/index.css";
 import { MdPreview } from "md-editor-v3";
 import "md-editor-v3/lib/preview.css";
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App);
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+
 app.use(router);
 app.use(Element);
 app.use("MdPreview", MdPreview);
