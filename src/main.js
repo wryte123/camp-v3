0df317ddc9557342bf2f6501952d5ef067de699c
wryte <oepkf@foxmail.com>
2024-03-29 21:55:40 +0800

    1

--- src/main.js
+++ src/main.js
@@ -7,19 +7,19 @@ import "element-plus/theme-chalk/index.css";
 import "./styles/el.scss";
 import { MdPreview } from "md-editor-v3";
 import "md-editor-v3/lib/preview.css";
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 import requestService from "./scripts/Requests.js";
-
-
 
 const app = createApp(App);
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-    app.component(key, component)
+  app.component(key, component);
 }
 
 app.use(router);
 app.use(Element);
 app.component("MdPreview", MdPreview);
-app.mount("#app");
 
+// 在这里设置 Vue 原型属性
 Vue.prototype.instance = requestService;
+
+app.mount("#app");
